314889d67f2284f29e7600eef087b25bc64c5597
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-05-29 23:39:28 -0400

    Create sortInDecreasingOrder.js

---
+++ DataTypes-ArraysMethods/sortInDecreasingOrder.js
@@ -0,0 +1,11 @@
+// Order an Array in decreasing order
+// My solution:
+
+function superSort(arr) {
+	return arr.sort((a, b) => b - a);
+}
+
+//* Test
+// superSort([5, 2, 1, -10, 8])
+//* Output
+// [ 8, 5, 2, 1, -10 ]
