9fd1985bae3008ab92974e05e2fe553586efd8e9
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-06-22 20:23:14 -0400

    Create workingWithVariables.js

---
+++ Fundamentals-Variables/workingWithVariables.js
@@ -0,0 +1,13 @@
+// Tasks
+// 1. Declare two variables: admin and name.
+let admin;
+let name;
+
+// 2. Assign the value "John" to name.
+name = 'John';
+
+// 3. Copy the value from name to admin.
+name = admin;
+
+// 4. Show the value of admin using alert (must output “John”).
+alert(admin);
