96dd30cd705cb3e12a644c84988f2e88fefacb9a
JuanPabloNSZ <juanpablo.nsz@gmail.com>
2022-10-10 21:47:16 -0300

    Create helloObject.js

---
+++ ObjectsTheBasics-Objects/helloObject.js
@@ -0,0 +1,19 @@
+/* Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object. */
+
+//* My solution:
+// Create an empty object user.
+let user = {};
+// Add the property name with the value John.
+user.name = 'John';
+// Add the property surname with the value Smith.
+user.surname = 'Smith';
+// Change the value of the name to Pete.
+user.name = 'Pete';
+// Remove the property name from the object.
+delete user.name;
